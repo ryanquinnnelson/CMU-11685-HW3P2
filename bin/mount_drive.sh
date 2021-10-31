66f152b5834eabc081e9f49756bfa14001ce7587
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-10-31 17:18:52 -0400

    Made drive location a parameter instead of hardcoded

--- bin/mount_drive.sh
+++ bin/mount_drive.sh
@@ -5,12 +5,18 @@
 # Follows the steps outlined on StackOverflow:
 # https://stackoverflow.com/questions/45167717/mounting-a-nvme-disk-on-aws-ec2/64709212#64709212
 
+if [ -z "$1" ]; then
+    echo "Drive must be supplied as the first argument: mount_drive.sh <drive>"
+    exit 1
+fi
+
+
 echo "Listing block devices..."
 lsblk
 echo
 
-# the output should be /dev/nvme1n1: data for empty drives
-sudo file -s /dev/nvme1n1
+# the output should be /dev/nvme1n1: data for empty drives on g ec2 instances
+sudo file -s $1
 
 
 if [[ $(sudo file -s /dev/nvme1n1 | grep -L ": data") ]]; then
@@ -19,11 +25,11 @@ else
 	echo "Drive is empty. Mounting drive..."
 
 	# format
-	sudo mkfs -t xfs /dev/nvme1n1
+	sudo mkfs -t xfs $1
 
 	# create a folder in current directory and mount the nvme drive
 	sudo mkdir /data
-	sudo mount /dev/nvme1n1 /data
+	sudo mount $1 /data
 	echo
 
 	# change owner and group
@@ -35,4 +41,5 @@ fi
 # check existence
 echo "Checking amount of free disk space available..."
 df -h
+
 
